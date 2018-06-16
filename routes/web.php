ebadb64c08d0715ad302f6a3e3d5bf4875fbdb43
lewisp <7692309+lewisp@users.noreply.github.com>
2018-06-15 22:36:05 -0400

    Add a route for showing an image

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/img', function () {
+	$banniere = App\Banniere::find(1);
+    return '<img src="data:image/jpeg;base64,' . base64_encode(hex2bin(substr($banniere->image, 2))) . '"/>';
+    // return $banniere;
+});
