fa6e1ad3ba471eb89f9e92d965b1570440bd4812
lewisp <7692309+lewisp@users.noreply.github.com>
2018-06-21 22:35:25 -0400

    Update factory and set the same password for all

--- database/factories/AdministrateurFactory.php
+++ database/factories/AdministrateurFactory.php
@@ -5,8 +5,8 @@ use Faker\Generator as Faker;
 $factory->define(App\Administrateur::class, function (Faker $faker) {
     return [
         'nom' => $faker->name,
-        'mot_de_passe' => $faker->sha256,
-        'courriel' => $faker->email,
+        'password' => Hash::make('password'), //$faker->sha256,
+        'email' => $faker->email,
         'adresse_id' => function () {
             return factory(App\Adresse::class)->create()->id;
         }
