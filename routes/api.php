e54ed1aa55a2db7567134b800098c8bf86a12356
Mousselmal Akram <ak4am.mo@gmail.com>
2018-07-03 15:02:07 -0400

    update patch request profil-cible

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ Route::group([
 	Route::post('profils', 'ProfilController@store');
 	Route::get('profils', 'ProfilController@index');
 	Route::get('profils/{id}', 'ProfilController@show');
-    Route::put('profils/{id}', 'ProfilController@update');
+    Route::patch('profils/{id}', 'ProfilController@update');
     Route::delete('profils/{id}', 'ProfilController@destroy');
 });
 
