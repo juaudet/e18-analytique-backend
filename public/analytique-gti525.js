c59fed3dbeab641f03a8cb65630968cd8fcdc749
lewisp <7692309+lewisp@users.noreply.github.com>
2018-07-14 22:01:50 -0400

    Update analytique script for the page tracking

--- public/analytique-gti525.js
+++ public/analytique-gti525.js
@@ -1,8 +1,9 @@
 (function () {
 
-    function AnalytiqueGTI525(domain) {
+    function AnalytiqueGTI525(domain, token) {
 
         var domain = domain;
+        var token = token;
 
         this.banniere = function (selector, format) {
             var xhr = new XMLHttpRequest();
@@ -18,11 +19,19 @@
             }
         }
 
+        this.page = function (url) {
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', domain + '/api/page?token=' + token + '&url=' + url);
+            xhr.withCredentials = true;
+            xhr.send();
+        }
+
     };
 
-    window[_var] = new AnalytiqueGTI525(_dom);
+    window[_var] = new AnalytiqueGTI525(_dom, _tok);
     delete _var;
     delete _dom;
+    delete _tok;
 
 })();
 
